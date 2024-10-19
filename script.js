2983f03cb6d7026889e7f77f7cf8091b0a33b97e
sci-punk <ishen.sarabia@gmail.com>
2024-10-18 22:55:19 -0600

    Add logo and password check

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const form = document.querySelector('form');
+    const submitBtn = document.querySelector('.submit-btn');
     const password = document.querySelector('#password');
     const confirmPassword = document.querySelector('#confirm-password');
     const errorMessage = document.querySelector('.error');
 
-    form.addEventListener('submit', (e) => {
+    submitBtn.addEventListener('click', (e) => {
         if (password.value !== confirmPassword.value) {
             e.preventDefault();
             errorMessage.style.visibility = "visible";
-            errorMessage.textContent = "Passwords do not match!";
+            errorMessage.textContent = "*Passwords do not match!";
+
+            //Change the border color of the input fields
+            password.style.borderColor = "red";
+            confirmPassword.style.borderColor = "red";
         } else {
             errorMessage.style.visibility = "hidden";
             errorMessage.textContent = "";
